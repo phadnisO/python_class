b5939b3438dcb551d3eda930ae1b03b34017d8f7
phadnisO <76860380+phadnisO@users.noreply.github.com>
2021-01-17 05:24:49 +0000

    Update and rename README.md to Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,28 @@
+# python_class
+Player = []
+Player.append("ajinkya")
+Player.append("virat")
+Player.append("jadeja")
+Player.append("bumrah")
+Player.append("bhuvneshvar")
+for pl in Player:
+  print(pl)
+for pl in Player:
+  if pl== "bhuvneshvar":
+    print ("Valid Player")
+  else:
+    print ("non valid Player")
+    
+ # Results
+Player.append("ajinkya")
+Player.append("virat")
+Player.append("jadeja")
+Player.append("bumrah")
+Player.append("bhuvneshvar")
+for pl in Player:
+  print(pl)
+for pl in Player:
+  if pl== "bhuvneshvar":
+    print ("Valid Player")
+  else:
+    print ("non valid Player")
